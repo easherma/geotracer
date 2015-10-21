5b67592547db7d68bcffc2fbf85c2c6a3889aa72
Patrick Sier <pjsier@gmail.com>
2015-10-21 17:05:49 -0500

    loop through multiple points if more than one

--- views.py
+++ views.py
@@ -15,8 +15,6 @@ def index():
                 "ST_AsGeoJSON(p4) as p4," +
                 "ST_AsGeoJSON(p5) as p5 " +
                 "FROM geopaths;")
-        print "success"
-        print carto_geoj
     except CartoDBException as e:
         print("some error ocurred", e)
     return render_template('index.html', carto_geoj=carto_geoj)
@@ -24,12 +22,12 @@ def index():
 @app.route('/geo', methods=["POST"])
 def geodata():
     # Query: INSERT INTO geopaths (the_geom) VALUES (ST_SetSRID(ST_Point(" + coords[0].toString() + ", " + coords[1].toString() + "),4326))
-    print "Received"
-    print request.json[0]
     cl = CartoDBAPIKey(keys.cartodb_key, keys.cartodb_user)
-    try:
-        cl.sql("INSERT INTO geopaths (the_geom) VALUES (ST_SetSRID(ST_Point(" +
-        str(request.json[0][0]) + ", " + str(request.json[0][1]) + "),4326))")
-    except CartoDBException as e:
-        print("some error ocurred", e)
-    return render_template('index.html')
+    geodata = request.json
+    for geo in geodata:
+        try:
+            cl.sql("INSERT INTO geopaths (the_geom) VALUES (ST_SetSRID(ST_Point(" +
+            str(geo[0]) + ", " + str(geo[1]) + "),4326))")
+        except CartoDBException as e:
+            print("some error ocurred", e)
+    return redirect(url_for('index'))
