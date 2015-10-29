ed469a59a022602710e54c3aa98ac87a55d54f6d
Eric Sherman <ericandrewsherman@gmail.com>
2015-10-29 15:13:50 -0500

    minor edits
    playing with JS and flask interaction

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask import render_template
 import os
-app = Flask(__name__)
-
+import keys
 #enviornmentalvariablesscriptexportbeforeyourunpython
 from flask import render_template, request, redirect, url_for, jsonify
 from cartodb import CartoDBAPIKey, CartoDBException
 import json 
 
+app = Flask(__name__)
 cartodb_key = os.environ.get("cartodb_key")
 cartodb_user= os.environ.get("cartodb_user")
 
@@ -16,8 +16,9 @@ def index():
     print("HELLO FROM INDEX")
     cl = CartoDBAPIKey(cartodb_key, cartodb_user)
     try:
-        carto_geoj = cl.sql("SELECT cartodb_id, ST_AsGeoJSON(p1) as p1, ST_AsGeoJSON(p2) as p2 FROM geopaths WHERE p1 IS NOT NULL;")
+        carto_geoj = cl.sql("SELECT cartodb_id, ST_AsGeoJSON(p1) as p1, ST_AsGeoJSON(p2) as p2 FROM geopaths;")
         last_row_id = max([row['cartodb_id'] for row in carto_geoj['rows']])
+        print(carto_geoj)
         print("Length of database is: ", len(carto_geoj['rows']))
     except CartoDBException as e:
         print("some error ocurred", e)
@@ -28,6 +29,7 @@ def geodata():
     # Query: INSERT INTO geopaths (the_geom) VALUES (ST_SetSRID(ST_Point(" + coords[0].toString() + ", " + coords[1].toString() + "),4326))
     cl = CartoDBAPIKey(cartodb_key, cartodb_user)
     geodata = request.json
+    print(geodata)
     for index in range(1, len(geodata)):
         try:
             cl.sql("INSERT INTO geopaths (p1, p2) VALUES (ST_SetSRID(ST_Point(" +
@@ -50,6 +52,8 @@ def update():
     except CartoDBException as e:
         print("some error occurred", e)
     return jsonify(carto_geoj)
+
+    
  
 if __name__ == '__main__':
    app.run(debug=True)
