73aebb03d67b26faad9f4a69b43221e3b40f8600
Eric Chandler <eric.chandler@alumni.cmu.edu>
2015-10-31 16:35:14 -0500

    clean up after merge

--- static/js/geopaths.js
+++ static/js/geopaths.js
@@ -1,78 +1,44 @@
+/* ************ MAIN ************ */
 
-//leaflet has layer toggles, could have one map, button toggles the main layer off, cumaltive submissions from user
 // Declare global variables
 var coord_array = []
 var UPDATE_INTERVAL = 30000; //unis of ms
-<<<<<<< HEAD
-var geocoderResults;
-//marker array
-=======
+
 //Create new grouping of non-user-submitted paths.
 //We will add paths to the group as they are retrieved from the db.
 var strangers_layer_group = L.layerGroup();
->>>>>>> origin/master
+
 // Show the whole world in this first view.
 var map = L.map('map', {
-    bounceAtZoomLimits: true,
-	 maxBounds:
-	 [[-85.0, -180.0],
-        [85.0, 180.0]],
-    inertia: false,
+  bounceAtZoomLimits: true,
+  maxBounds: [[-85.0, -180.0],[85.0, 180.0]],
+  inertia: false,
 	minZoom: 2,
 	continuousWorld: false,
 	layers: [strangers_layer_group]
 }).setView([20, 0], 2);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-// LOOKS LIKE MAPZEN KEY HAS TO BE ON FRONT END
-var options =
-{
-  position: 'topright'
-}
-var geocoder =  L.control.geocoder('search-daf-vyw', options).addTo(map);
 
-// Transform a GeoJSON string '{type:"Point",coordinates:[x,y]}'
-// into a leaflet LatLng object
-function geoJSONToLLatLng(ptStr) {
-  var p = JSON.parse(ptStr);
-  return new L.LatLng(p.coordinates[1],p.coordinates[0]);
-}
-
-<<<<<<< HEAD
-// REMOVED KEYS
-// REMOVED LOADING FROM CARTODB DIRECTLY TO AVOID HAVING KEYS ON FRONT END
-/*geoj.rows.forEach(function(row,i){
-  if(row.p1 != null && row.p2 != null){
-    var latlngs = [geoJSONToLLatLng(row.p1),geoJSONToLLatLng(row.p2)];
-    var line = L.polyline(latlngs,{snakingSpeed: 200}); 
-    line.addTo(map).snakeIn();
-  }
-})*/
+var geocoder_options = {position: 'topright'};
+var geocoder =  L.control.geocoder('search-daf-vyw', geocoder_options).addTo(map);
 
 
-
+// TODO: Choose: Add geojson straight to map or to layercontrol
 var geojsonFeature = geoj
 L.geoJson(geojsonFeature).addTo(map);
+geojsonFeature.addTo(map).snakeIn();
 
-=======
 // this loads data and does the inital animation
->>>>>>> origin/master
 geoj.rows.forEach(function(row,i){
   if(row.p1 != null && row.p2 != null){
     var latlngs = [geoJSONToLLatLng(row.p1),geoJSONToLLatLng(row.p2)];
     var line = L.polyline(latlngs,{snakingSpeed: 200}); 
     //add to layergroup instead of directly to map
-	strangers_layer_group.addLayer(line);
-	//line.addTo(map).snakeIn();
+	  strangers_layer_group.addLayer(line);
+	  //line.addTo(map).snakeIn();
   }
 })
 
-<<<<<<< HEAD
-geojsonFeature.addTo(map).snakeIn();
-//Gets new rows from the server and plots them.
-//update_map executes periodically and indefinitely until server returns error
-// It is also asynchronous, so control moves past this line
-//comment this for debugging
-=======
 //Create leaflet control to toggle map layers
 var overlayMaps = {
   "strangers": strangers_layer_group,
@@ -89,8 +55,20 @@ overlayMaps.strangers.eachLayer(function(x){x.snakeIn()});
 //Gets new rows from the server and plots them.
 //update_map executes periodically and indefinitely until server returns error
 // It is also asynchronous, so control moves past this line
->>>>>>> origin/master
-//update_map();
+//update_map(); //commented out while backend is in flux
+
+//commented out while front end is in flux
+//document.getElementById("submit_button").addEventListener("click", post_array);
+
+
+/* ************ FUNCTIONS *********** */
+
+// Transform a GeoJSON string '{type:"Point",coordinates:[x,y]}'
+//  into a leaflet LatLng object  */
+function geoJSONToLLatLng(ptStr) {
+  var p = JSON.parse(ptStr);
+  return new L.LatLng(p.coordinates[1],p.coordinates[0]);
+}
 
 //Gets new rows from the server and plots them.
 // Executes periodically and indefinitely until server returns an error.
@@ -161,8 +139,3 @@ function post_array() {
   }
 };
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
-//document.getElementById("submit_button").addEventListener("click", post_array);
