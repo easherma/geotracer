73ad0565b0f277d9d52b3335896aa918c7939d81
Vinesh Kannan <vingkan@gmail.com>
2015-11-21 00:52:51 -0600

    added revealTextarea()

--- static/js/map-ui-scripts.js
+++ static/js/map-ui-scripts.js
@@ -1,6 +1,13 @@
+function revealTextarea(id, placeholderText){
+	var placeholder = placeholderText || "Enter a note here.";
+	document.getElementById(id).value = placeholder;
+	$('#' + id).addClass('visible');
+}
+
 function addTextToPoint(coordPair, place){
 	console.log("PLACE:");
 	console.log(place);
+	revealTextarea('text-map-addition');
 }
 
 console.log("LOADED map-ui-scripts.js");
