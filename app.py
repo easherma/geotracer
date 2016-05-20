a7f77795dd95748e67b7d611247ff6fa3bdf4920
Eric Sherman <ericandrewsherman@gmail.com>
2016-05-20 15:50:23 -0500

    custom test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import views
 import os
 from flask import render_template, request, redirect, url_for, jsonify
 from cartodb import CartoDBAPIKey, CartoDBException
-import json 
+import json
 #import keys
 
 
@@ -12,11 +12,13 @@ app = Flask(__name__)
 cartodb_key = os.environ.get("cartodb_key")
 cartodb_user= os.environ.get("cartodb_user")
 
+
+
 @app.route('/')
 def index():
     cl = CartoDBAPIKey('',cartodb_user)
     try:
-        carto_geoj = cl.sql("SELECT * FROM points ORDER BY random() LIMIT 1;", format='geojson')
+         carto_geoj = cl.sql("SELECT * FROM points ORDER BY random() LIMIT 1;", format='geojson')
         id = carto_geoj['features'][0]['properties']['cartodb_id']
         #TODO: Parse array of strings, not array of objects as place labels
         labels_resp = cl.sql("SELECT pelias_label FROM points WHERE cartodb_id = %s ;" % id)
@@ -26,16 +28,17 @@ def index():
 
     except CartoDBException as e:
         print("some error ocurred", e)
-    return render_template('index.html', 
-                           carto_geoj=json.dumps(carto_geoj), 
+    return render_template('index.html',
+                           carto_geoj=json.dumps(carto_geoj),
                            carto_places=labels,
                            last_row_id=last_row_id, id=id)
-                           
+
 @app.route('/all')
 def all():
     cl = CartoDBAPIKey('',cartodb_user)
     try:
         carto_geoj = cl.sql("SELECT * FROM points;", format='geojson')
+
         id = "All"
         #TODO: Parse array of strings, not array of objects as place labels
         labels_resp = cl.sql("SELECT pelias_label FROM points ")
@@ -45,11 +48,30 @@ def all():
 
     except CartoDBException as e:
         print("some error ocurred", e)
-    return render_template('index.html', 
-                           carto_geoj=json.dumps(carto_geoj), 
+    return render_template('index.html',
+                           carto_geoj=json.dumps(carto_geoj),
                            carto_places=labels,
                            last_row_id=last_row_id, id=id)
-						   
+@app.route('/custom')
+def custom():
+    cl = CartoDBAPIKey('',cartodb_user)
+    try:
+        carto_geoj = cl.sql("SELECT * FROM points WHERE group_id = '1';", format='geojson')
+
+        id = "Custom"
+        #TODO: Parse array of strings, not array of objects as place labels
+        labels_resp = cl.sql("SELECT pelias_label FROM points ")
+        labels = [[y for y in json.loads(x['pelias_label'])] for x in labels_resp['rows']]
+        last_row_id_resp = cl.sql("SELECT MAX(cartodb_id) AS id FROM points")
+        last_row_id = last_row_id_resp['rows'][0]['id']
+
+    except CartoDBException as e:
+        print("some error ocurred", e)
+    return render_template('index.html',
+                           carto_geoj=json.dumps(carto_geoj),
+                           carto_places=labels,
+                           last_row_id=last_row_id, id=id)
+
 @app.route('/geo', methods=['GET', 'POST'])
 def geodata():
     cl = CartoDBAPIKey(cartodb_key, cartodb_user)
@@ -99,10 +121,10 @@ def index_s(id):
 
    except CartoDBException as e:
        print("some error ocurred", e)
-   return render_template('index.html', 
-                          carto_geoj=carto_geoj, 
+   return render_template('index.html',
+                          carto_geoj=carto_geoj,
                           carto_places=labels,
-                          last_row_id=last_row_id, id=id)   
- 
+                          last_row_id=last_row_id, id=id)
+
 if __name__ == '__main__':
    app.run(debug=True)
