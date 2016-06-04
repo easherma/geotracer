5489dfbd477aa9f17d3e162a07ebf56cf9d2446c
Eric Sherman <ericandrewsherman@gmail.com>
2016-06-03 19:19:58 -0500

    basic frontend work
    pushes to geojson, snake animation works, storing locally for now. no
    backend = lolz

--- L.Polyline.SnakeAnim.js
+++ L.Polyline.SnakeAnim.js
@@ -1,9 +1,3 @@
-/*
- * "THE BEER-WARE LICENSE": ivan@sanchezortega.es wrote this file.
- * As long as you retain this notice you can do whatever you want 
- * with this stuff. If we meet some day, and you think this stuff is
- * worth it, you can buy me a beer in return.
-*/
 
 
 
@@ -142,7 +136,7 @@ L.Polyline.include({
 
 
 L.Polyline.mergeOptions({
-	snakingSpeed: 275	// In pixels/sec
+	snakingSpeed: 200	// In pixels/sec
 });
 
 
